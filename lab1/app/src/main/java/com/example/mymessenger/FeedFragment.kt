c9bfcd3a68928c23fa45b018bee1cf4bba082c67
lina-dmitrieva <balerinamalina49@gmail.com>
2026-01-25 23:52:37 +0300

    lab-1

---
+++ lab1/app/src/main/java/com/example/mymessenger/FeedFragment.kt
@@ -0,0 +1,37 @@
+package com.example.mymessenger
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FeedFragment : Fragment() {
+
+    private val TAG = "FeedFragment"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        Log.d(TAG, "onCreateView")
+        return inflater.inflate(R.layout.fragment_feed, container, false)
+    }
+
+    override fun onDestroyView() {
+        Log.d(TAG, "onDestroyView")
+        super.onDestroyView()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy")
+        super.onDestroy()
+    }
+}
